38c178970eae8e2aaea01a6a2259ebf137bb25b2
Ryan Scarff <williamscarff@airbears2-10-142-155-69.airbears2.1918.berkeley.edu>
2018-03-07 10:56:23 -0800

    set up models and associations with models

---
+++ models/comment.js
@@ -0,0 +1,21 @@
+module.exports = function(sequelize, DataTypes) {
+	var Comment = sequelize.define("Comment", {
+		body: {
+			type: DataTypes.TEXT,
+			allowNull: false,
+			validate: {
+				len: [1]
+			}
+		}
+	});
+
+	Comment.associate = function(models) {
+
+		// A comment has an associated post and cannot be created without an associated post.
+		Comment.belongsTo(models.Post, {
+			foreignKey: {
+				allowNull: false
+			}
+		})
+	}
+}
---
+++ schema.sql
@@ -0,0 +1,2 @@
+DROP DATABASE IF EXISTS gitlabspot_db;
+CREATE DATABASE gitlabspot_db;
