f7ecc53ca9c4a8a0c5faf8c5901f332bb356e9c1
paulakedouk <paulakedouk@gmail.com>
2018-03-07 14:02:51 -0800

    Testing

---
+++ public/index.js
@@ -0,0 +1,21 @@
+$('#cms').on('submit', function() {
+  var title = $('#title')
+    .val()
+    .trim();
+  var body = $('#body')
+    .val()
+    .trim();
+  var category = $('#category')
+    .val()
+    .trim();
+  var newPost = {
+    title: title,
+    body: body,
+    category: category,
+    UserID: 1
+  };
+
+  $.post('/api/post', newPost, function() {
+    window.location.reload();
+  });
+});
