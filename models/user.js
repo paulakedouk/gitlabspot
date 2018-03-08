2c6ecc2380b783f207d9e123f8f7cb8aa1811ea9
Jose <Perez@Joses-MacBook-Air.local>
2018-03-08 11:43:52 -0800

    fixed bug with sign up form

--- models/user.js
+++ models/user.js
@@ -1,16 +1,62 @@
-module.exports = function(sequelize, DataTypes) {
-  var User = sequelize.define("User", {
-    // Giving the User model a name of type STRING
-    name: DataTypes.STRING
-  });
-
-  User.associate = function(models) {
+module.exports = function(sequelize, Sequelize) {
+ 
+    var User = sequelize.define('user', {
+ 
+        id: {
+            autoIncrement: true,
+            primaryKey: true,
+            type: Sequelize.INTEGER
+        },
+ 
+        firstname: {
+            type: Sequelize.STRING,
+            notEmpty: true
+        },
+ 
+        lastname: {
+            type: Sequelize.STRING,
+            notEmpty: true
+        },
+ 
+        username: {
+            type: Sequelize.TEXT
+        },
+ 
+        about: {
+            type: Sequelize.TEXT
+        },
+ 
+        email: {
+            type: Sequelize.STRING,
+            validate: {
+                isEmail: true
+            }
+        },
+ 
+        password: {
+            type: Sequelize.STRING,
+            allowNull: false
+        },
+ 
+        last_login: {
+            type: Sequelize.DATE
+        },
+ 
+        status: {
+            type: Sequelize.ENUM('active', 'inactive'),
+            defaultValue: 'active'
+        }
+ 
+ 
+    });
+    User.associate = function(models) {
     // Associating User with Posts
     // When an User is deleted, also delete any associated Posts
     User.hasMany(models.Post, {
       onDelete: "cascade"
     });
   };
-
-  return User;
-};
+ 
+    return User;
+ 
+}
