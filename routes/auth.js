600c22680888cf7a7c5b021d3d0592f39a19c904
Jose <Perez@Joses-MacBook-Air.local>
2018-03-12 13:30:48 -0700

    fixing signup page and posting to database

--- routes/auth.js
+++ routes/auth.js
@@ -2,11 +2,18 @@ var authController = require('../controllers/authcontroller.js');
 //added require passport(not in tutorial)
 var passport = require('passport');
 //added passport to the parameter(not in tutorial)
+
 module.exports = function (app) {
   // app.get('/signup', authController.signup);
   // app.get('/signin', authController.signin);
   // // app.get('/dashboard', isLoggedIn, authController.dashboard);
   // app.get('/logout', authController.logout);
+
+  function isLoggedIn(req, res, next) {
+    if (req.isAuthenticated()) return next();
+
+    res.redirect('/signin');
+  }
 
   app.get("/signin", function (req, res) {
     res.render("signin")
@@ -16,7 +23,7 @@ module.exports = function (app) {
     res.render("signup")
   })
 
-  app.get("/dashboard", function (req, res) {
+  app.get("/dashboard", isLoggedIn, function (req, res) {
     res.render("dashboard")
   })
 
@@ -37,10 +44,4 @@ module.exports = function (app) {
       failureRedirect: '/signin'
     })
   );
-
-  function isLoggedIn(req, res, next) {
-    if (req.isAuthenticated()) return next();
-
-    res.redirect('/signin');
-  }
 };
