2604cb21881efaeb32da8bf6cba764ea5d7a876f
Ryan Scarff <rscarff@berkeley.edu>
2018-03-12 20:32:03 -0700

    app.js has get request now

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,4 +1,4 @@
-// $(document).ready(function(){
+
 $(".create-submit").click(function(){
 	var newPost = {
 		title: $("#post-title").val().trim(),
@@ -10,6 +10,24 @@ $(".create-submit").click(function(){
             })
 })
 
-
-
-// })
+$(document).ready(function(){
+	$.get("/api/category/HTML", function(data) {
+		if (data) {
+			var newDiv = $("<div>");
+			var postTitle = data.title;
+			var postDate = data.updatedAt;
+			var postBody = data.body;
+			var postCreator = data.username
+			var titleDiv = $("<div class=post-title>");
+			var dateDiv = $("<div class=post-date>");
+			var bodyDiv = $("<p>");
+			titleDiv.append(postTitle);
+			dateDiv.append(postDate);
+			bodyDiv.append(postBody);
+			newDiv.append(titleDiv);
+			newDiv.append(dateDiv);
+			newDiv.append(bodyDiv);
+			$(".col-lg-12").append(newDiv);
+		}
+	})
+})
